88223db6e5a9c84e25bb5de4a590da00a4fa0280
Pedro Arcoverde <pedroarcoverde2@gmail.com>
2022-12-11 22:28:47 -0300

    lista 3

--- Lista 3/q15.py
+++ Lista 3/q15.py
@@ -1,11 +1,12 @@
-n = int(input("Digite o valor de n: "))
-aux = 0#auxiliar para a progressão aritimetica de segundo grau
-raz = 1#razão da Pa de segundo grau
+n = int(input('Informe o numero: '))
+aux = 0
+cont = 1
+cont2 = 0
 while aux <= n:
-    aux = raz + aux
-    raz +=1 
-    #caso a PA seja igual ao N é triangular, mas se não for, não é triangular
+    aux += cont 
+    cont += 1
     if aux == n:
-        print(f"o numero {n} é triangular")
-    else:
-        print(f"o numero {n} não é triangular")
+        print(f'{n} é um Triangular!')
+        cont2 += 1
+if cont2 == 0:
+    print(f'{n} não é um Triangular!')
