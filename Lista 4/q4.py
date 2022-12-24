779417314bb6df239c2bf9a1cb046877044687f5
Pedro Arcoverde <pedroarcoverde2@gmail.com>
2022-12-23 22:03:21 -0300

    lista 4

---
+++ Lista 4/q4.py
@@ -0,0 +1,8 @@
+'''codigo sem utilizar while
+nm1 = input("coloque um nome ou um numero ")
+nm2 = nm1[::-1]
+nmaux1 = nm1.replace(" ", "")
+nmaux2 = nm2.replace(" ", "")
+if nmaux1.upper() == nmaux2.upper():
+    print (f"{nm1}, {nm2} é palindromos")
+else: print ('não é um palindromo')'''
